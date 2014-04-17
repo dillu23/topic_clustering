e28ddc0130de0fdb2c0c3be687a7d6e55d9340ff
dillu23 <dilpreet023@gmail.com>
2014-04-17 13:00:57 +0530

    changes

--- text.py
+++ text.py
@@ -1597,3 +1597,421 @@ class onlineTfidfVectorizer(BaseEstimator, VectorizerMixin):
             "The 'stop_words_ attribute was renamed to 'max_df_stop_words'. "
             "The old attribute will be removed in 0.15.", DeprecationWarning)
         return self.stop_words_
+
+class onlineTfidfVectorizer2(BaseEstimator, VectorizerMixin):
+    """Convert a collection of text documents to a matrix of token counts
+
+    This implementation produces a sparse representation of the counts using
+    scipy.sparse.coo_matrix.
+
+    If you do not provide an a-priori dictionary and you do not use an analyzer
+    that does some kind of feature selection then the number of features will
+    be equal to the vocabulary size found by analysing the data. The default
+    analyzer does simple stop word filtering for English.
+
+    Parameters
+    ----------
+    input : string {'filename', 'file', 'content'}
+        If filename, the sequence passed as an argument to fit is
+        expected to be a list of filenames that need reading to fetch
+        the raw content to analyze.
+
+        If 'file', the sequence items must have 'read' method (file-like
+        object) it is called to fetch the bytes in memory.
+
+        Otherwise the input is expected to be the sequence strings or
+        bytes items are expected to be analyzed directly.
+
+    charset : string, 'utf-8' by default.
+        If bytes or files are given to analyze, this charset is used to
+        decode.
+
+    charset_error : {'strict', 'ignore', 'replace'}
+        Instruction on what to do if a byte sequence is given to analyze that
+        contains characters not of the given `charset`. By default, it is
+        'strict', meaning that a UnicodeDecodeError will be raised. Other
+        values are 'ignore' and 'replace'.
+
+    strip_accents : {'ascii', 'unicode', None}
+        Remove accents during the preprocessing step.
+        'ascii' is a fast method that only works on characters that have
+        an direct ASCII mapping.
+        'unicode' is a slightly slower method that works on any characters.
+        None (default) does nothing.
+
+    analyzer : string, {'word', 'char', 'char_wb'} or callable
+        Whether the feature should be made of word or character n-grams.
+        Option 'char_wb' creates character n-grams only from text inside
+        word boundaries.
+
+        If a callable is passed it is used to extract the sequence of features
+        out of the raw, unprocessed input.
+
+    preprocessor : callable or None (default)
+        Override the preprocessing (string transformation) stage while
+        preserving the tokenizing and n-grams generation steps.
+
+    tokenizer : callable or None (default)
+        Override the string tokenization step while preserving the
+        preprocessing and n-grams generation steps.
+
+    ngram_range : tuple (min_n, max_n)
+        The lower and upper boundary of the range of n-values for different
+        n-grams to be extracted. All values of n such that min_n <= n <= max_n
+        will be used.
+
+    stop_words : string {'english'}, list, or None (default)
+        If a string, it is passed to _check_stop_list and the appropriate stop
+        list is returned. 'english' is currently the only supported string
+        value.
+
+        If a list, that list is assumed to contain stop words, all of which
+        will be removed from the resulting tokens.
+
+        If None, no stop words will be used. max_df can be set to a value
+        in the range [0.7, 1.0) to automatically detect and filter stop
+        words based on intra corpus document frequency of terms.
+
+    lowercase : boolean, default True
+        Convert all characters to lowercase befor tokenizing.
+
+    token_pattern : string
+        Regular expression denoting what constitutes a "token", only used
+        if `tokenize == 'word'`. The default regexp select tokens of 2
+        or more letters characters (punctuation is completely ignored
+        and always treated as a token separator).
+
+    max_df : float in range [0.0, 1.0] or int, optional, 1.0 by default
+        When building the vocabulary ignore terms that have a term frequency
+        strictly higher than the given threshold (corpus specific stop words).
+        If float, the parameter represents a proportion of documents, integer
+        absolute counts.
+        This parameter is ignored if vocabulary is not None.
+
+    min_df : float in range [0.0, 1.0] or int, optional, 2 by default
+        When building the vocabulary ignore terms that have a term frequency
+        strictly lower than the given threshold. This value is also called
+        cut-off in the literature.
+        If float, the parameter represents a proportion of documents, integer
+        absolute counts.
+        This parameter is ignored if vocabulary is not None.
+
+    max_features : optional, None by default
+        If not None, build a vocabulary that only consider the top
+        max_features ordered by term frequency across the corpus.
+
+        This parameter is ignored if vocabulary is not None.
+
+    binary : boolean, False by default.
+        If True, all non zero counts are set to 1. This is useful for discrete
+        probabilistic models that model binary events rather than integer
+        counts.
+
+    dtype : type, optional
+        Type of the matrix returned by fit_transform() or transform().
+    
+    norm : 'l1', 'l2' or None, optional
+        Norm used to normalize term vectors. None for no normalization.
+
+    use_idf : boolean, optional
+        Enable inverse-document-frequency reweighting.
+
+    smooth_idf : boolean, optional
+        Smooth idf weights by adding one to document frequencies, as if an
+        extra document was seen containing every term in the collection
+        exactly once. Prevents zero divisions.
+
+    sublinear_tf : boolean, optional
+        Apply sublinear tf scaling, i.e. replace tf with 1 + log(tf).
+
+    Attributes
+    ----------
+    `vocabulary_` : dict
+        A mapping of terms to feature indices.
+
+    `stop_words_` : set
+        Terms that were ignored because
+        they occurred in either too many
+        (`max_df`) or in too few (`min_df`) documents.
+        This is only available if no vocabulary was given.
+    
+    'idf_' :  dict
+        A mapping of terms to inverse frequency score
+    
+    'document_counts_' :  dict
+        A mapping of terms to number of documents it occurs in.
+    
+    #TODO  sublinear_tf not implemented
+    See also
+    --------
+    HashingVectorizer, TfidfVectorizer
+    """
+    #TODO look into the parameters
+    def __init__(self, input='content', charset='utf-8',
+                 charset_error='strict', strip_accents=None,
+                 lowercase=True, preprocessor=None, tokenizer=None,
+                 stop_words=None, token_pattern=ur"(?u)\b\w\w+\b",
+                 ngram_range=(1, 1),
+                 min_n=None, max_n=None, analyzer='word',
+                 max_df=1.0, min_df=2, max_features=None,
+                 vocabulary=None, binary=False, dtype=float,
+                 norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False,
+                 min_dict_size= 1000):
+        self.input = input
+        self.charset = charset
+        self.charset_error = charset_error
+        self.strip_accents = strip_accents
+        self.preprocessor = preprocessor
+        self.tokenizer = tokenizer
+        self.analyzer = analyzer
+        self.lowercase = lowercase
+        self.token_pattern = token_pattern
+        self.stop_words = stop_words
+        self.max_df = max_df
+        self.min_df = min_df
+        self.max_features = max_features
+        if not (max_n is None) or not (min_n is None):
+            warnings.warn('Parameters max_n and min_n are deprecated. Use '
+                          'ngram_range instead. This will be removed in 0.14.',
+                          DeprecationWarning, stacklevel=2)
+            if min_n is None:
+                min_n = 1
+            if max_n is None:
+                max_n = min_n
+            ngram_range = (min_n, max_n)
+        self.ngram_range = ngram_range
+        
+        self.idf_ = {}
+        self.vocabulary_= {}
+        self.total_count = 0
+        self.document_counts = Counter()
+        self.binary = binary
+        self.dtype = dtype
+        self.norm = norm
+        self.use_idf = use_idf
+        self.smooth_idf = smooth_idf
+        self.sublinear_tf = sublinear_tf
+        self.min_dict_size = min_dict_size
+    
+    def _term_count_dicts_to_matrix(self, term_count_dicts):
+        i_indices = []
+        j_indices = []
+        values = []
+        vocabulary = self.vocabulary_
+        idf_scores = self.idf_
+        for i, term_count_dict in enumerate(term_count_dicts):
+            for term, count in term_count_dict.iteritems():
+                j = vocabulary.get(term)
+                if j is not None:
+                    i_indices.append(i)
+                    j_indices.append(j)
+                    ct = count
+                    if (self.binary and ct > 0):
+                            ct = 1
+                    if (self.sublinear_tf):
+                        ct = np.log(ct) + 1
+                    if(self.use_idf):
+                        values.append(float(ct) * idf_scores.get(term))
+                    else:
+                        values.append(count)
+            # free memory as we go
+            term_count_dict.clear()
+        shape = (i + 1, max(max(vocabulary.itervalues()) + 1, self.min_dict_size))
+        spmatrix = sp.coo_matrix((values, (i_indices, j_indices)),
+                                 shape=shape, dtype=self.dtype)
+        if self.binary:
+            spmatrix.data.fill(1)
+        if self.norm:
+            spmatrix = normalize(spmatrix, norm=self.norm, copy=False)
+        return spmatrix.tocsr()
+
+    def fit(self, raw_documents, y=None):
+        """Learn a vocabulary dictionary of all tokens in the raw documents.
+
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+
+        Returns
+        -------
+        self
+        """
+        self.fit_transform(raw_documents)
+        return self
+
+    def fit_transform(self, raw_documents, y=None):
+        """Learn the vocabulary dictionary and return the count vectors.
+
+        This is more efficient than calling fit followed by transform.
+
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+
+        Returns
+        -------
+        vectors : array, [n_samples, n_features]
+        """
+        self.vocabulary_ = {}
+        self.idf_ = {}
+        self.document_counts = Counter()
+        # result of document conversion to term count dicts
+        term_counts_per_doc = []
+        term_counts = Counter()
+
+        # term counts across entire corpus (count each term maximum once per
+        # document)
+        document_counts = Counter()
+
+        analyze = self.build_analyzer()
+        self.analyze2 = analyze
+        # TODO: parallelize the following loop with joblib?
+        # (see XXX up ahead)
+        for doc in raw_documents:
+            term_count_current = Counter(analyze(doc))
+            term_counts.update(term_count_current)
+
+            document_counts.update(term_count_current.iterkeys())
+
+            term_counts_per_doc.append(term_count_current)
+
+        n_doc = len(term_counts_per_doc)
+        max_features = self.max_features
+        max_df = self.max_df
+        min_df = self.min_df
+
+        max_doc_count = (max_df
+                         if isinstance(max_df, numbers.Integral)
+                         else max_df * n_doc)
+        min_doc_count = (min_df
+                         if isinstance(min_df, numbers.Integral)
+                         else min_df * n_doc)
+
+        # filter out stop words: terms that occur in almost all documents
+        if max_doc_count < n_doc or min_doc_count > 1:
+            stop_words = set(t for t, dc in document_counts.iteritems()
+                             if dc > max_doc_count or dc < min_doc_count)
+        else:
+            stop_words = set()
+
+        # list the terms that should be part of the vocabulary
+        if max_features is None:
+            terms = set(term_counts) - stop_words
+        else:
+            # extract the most frequent terms for the vocabulary
+            terms = set()
+            for t, tc in term_counts.most_common():
+                if t not in stop_words:
+                    terms.add(t)
+                if len(terms) >= max_features:
+                    break
+
+        # store the learned stop words to make it easier to debug the value of
+        # max_df
+        self.stop_words_ = stop_words
+        idf_score = dict()
+        
+        # store map from term name to feature integer index: we sort the term
+        # to have reproducible outcome for the vocabulary structure: otherwise
+        # the mapping from feature name to indices might depend on the memory
+        # layout of the machine. Furthermore sorted terms might make it
+        # possible to perform binary search in the feature names array.
+        
+        vocab = dict(((t, i) for i, t in enumerate(sorted(terms))))
+        if not vocab:
+            raise ValueError("empty vocabulary; training set may have"
+                             " contained only stop words or min_df (resp. "
+                             "max_df) may be too high (resp. too low).")
+        self.vocabulary_ = vocab
+        self.document_counts = document_counts
+        self.total_count = n_doc
+        for keyword in vocab:
+            idf_score[keyword] = np.log(float(n_doc + int(self.smooth_idf))/(document_counts[keyword] + int(self.smooth_idf))) + 1.0
+        self.idf_ = idf_score
+        # the term_counts and document_counts might be useful statistics, are
+        # we really sure want we want to drop them? They take some memory but
+        # can be useful for corpus introspection
+        return self._term_count_dicts_to_matrix(term_counts_per_doc)
+
+    def transform(self, raw_documents):
+        """Extract token counts out of raw text documents using the vocabulary
+        fitted with fit or the one provided in the constructor.
+
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+
+        Returns
+        -------
+        vectors : sparse matrix, [n_samples, n_features]
+        """
+        if not hasattr(self, 'vocabulary_') or len(self.vocabulary_) == 0 or len(self.idf_) ==0:
+            raise ValueError("Vocabulary wasn't fitted or is empty!")
+
+        # raw_documents can be an iterable so we don't know its size in
+        # advance
+
+        # XXX @larsmans tried to parallelize the following loop with joblib.
+        # The result was some 20% slower than the serial version.
+        analyze = self.analyze2
+        term_counts_per_doc = []
+        vocab = self.vocabulary_
+        document_count = Counter()
+        for doc in raw_documents:
+            term_count = Counter(analyze(doc))
+            term_counts_per_doc.append(term_count)
+            document_count.update(term_count.iterkeys())
+
+        for term in set(document_count) - set(vocab):
+            vocab[term] = len(vocab)
+        self.vocabulary_ = vocab
+        self.document_counts.update(document_count)
+        self.total_count = self.total_count + len(term_counts_per_doc)
+        for term in document_count:
+            self.idf_[term] = np.log(float(self.total_count + int(self.smooth_idf))/(self.document_counts[term] + int(self.smooth_idf))) + 1.0
+        return self._term_count_dicts_to_matrix(term_counts_per_doc)
+
+    def inverse_transform(self, X):
+        """Return terms per document with nonzero entries in X.
+
+        Parameters
+        ----------
+        X : {array, sparse matrix}, shape = [n_samples, n_features]
+
+        Returns
+        -------
+        X_inv : list of arrays, len = n_samples
+            List of arrays of terms.
+        """
+        if sp.isspmatrix_coo(X):  # COO matrix is not indexable
+            X = X.tocsr()
+        elif not sp.issparse(X):
+            # We need to convert X to a matrix, so that the indexing
+            # returns 2D objects
+            X = np.asmatrix(X)
+        n_samples = X.shape[0]
+
+        terms = np.array(self.vocabulary_.keys())
+        indices = np.array(self.vocabulary_.values())
+        inverse_vocabulary = terms[np.argsort(indices)]
+
+        return [inverse_vocabulary[X[i, :].nonzero()[1]].ravel()
+                for i in xrange(n_samples)]
+
+    def get_feature_names(self):
+        """Array mapping from feature integer indices to feature name"""
+        if not hasattr(self, 'vocabulary_') or len(self.vocabulary_) == 0:
+            raise ValueError("Vocabulary wasn't fitted or is empty!")
+
+        return [t for t, i in sorted(self.vocabulary_.iteritems(),
+                                     key=itemgetter(1))]
+
+    @property
+    def max_df_stop_words_(self):
+        warnings.warn(
+            "The 'stop_words_ attribute was renamed to 'max_df_stop_words'. "
+            "The old attribute will be removed in 0.15.", DeprecationWarning)
+        return self.stop_words_
